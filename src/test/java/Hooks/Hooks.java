3f4ca45f84b1e70b0cbf64de695f63df6b68bda9
Sowmithsow <93967866+Sowmithsow@users.noreply.github.com>
2026-06-02 16:35:47 +0530

    Update Hooks.java

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -1,29 +1,39 @@
 package Hooks;
+
 import java.time.Duration;
+
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 public class Hooks {
-	
-	public static WebDriver driver;
-	
-  @Before
-  public void setup() {
-	  driver=new ChromeDriver();
-		
-		driver.get("https://demowebshop.tricentis.com/");
-		
-		driver.manage().window().maximize();
-		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-  }
-  
-  @After 
-  public void close() {
-	  
-	  driver.quit();
-  }
+
+    public static WebDriver driver;
+
+    @Before
+    public void setup() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+
+        driver.get("https://demowebshop.tricentis.com/");
+
+        driver.manage().window().setSize(new Dimension(1920,1080));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After
+    public void close() {
+        driver.quit();
+    }
 }
